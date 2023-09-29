44298c1592b2b9c666b957fdffa5832617e22880
Bhumesh Gaur <113198254+BlackDevil559@users.noreply.github.com>
2023-09-29 12:39:49 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iogqlyaeajgbfymkdcwb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZ3FseWFlYWpnYmZ5bWtkY3diIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyODU2NTIsImV4cCI6MTk5ODg2MTY1Mn0.0Tv9LdzXboO7WryR5iDF7cxNAntI0TXCiFX_sJPV9Ng';
+const supabaseUrl = 'https://zynfbjjdegyxsysvrgjg.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bmZiampkZWd5eHN5c3ZyZ2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5Njg1OTgsImV4cCI6MjAxMTU0NDU5OH0.ngi7GqT4NBIPneZcROcL0lmUBpoG8EZFjINu9eaXAzU';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
